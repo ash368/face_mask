5f94d669ece80e0e3a1c3ca2a9b4d71cd59b3454
ash368 <64629004+ash368@users.noreply.github.com>
2020-05-02 00:59:53 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -62,7 +62,7 @@ async def analyze(request):
     
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
+    img = Image.open(BytesIO(img_bytes))
     
     
     def cli(pic_path ,save_pic_path ):
